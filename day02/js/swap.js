592a31d6bd601fd2ead3bb0c93c49ddf56555201
eddkim <ghddus456@gmail.com>
2021-11-09 12:51:05 +0900

    day02

---
+++ day02/js/swap.js
@@ -0,0 +1,9 @@
+var x = 0;
+var y = 1;
+document.write("---------교환 전-------<br>")
+document.write("x = "+ x +"<br>"+ "y = "+ y+"<br>")
+
+//교환 처리
+temp=x; x=y; y=temp;
+document.write("<br>---------교환 후-------<br>")
+document.write("x = "+ x +"<br>"+ "y = "+ y)
