92bb98f47c34da1cad0b02682d06e400b128e085
eddkim <ghddus456@gmail.com>
2021-11-16 12:48:49 +0900

    day07

---
+++ day07/js/event.js
@@ -0,0 +1,15 @@
+function showText(){
+    document.getElementById("desc").style.display="block";
+
+    //보기 버튼 숨기기
+document.getElementById("open").style.display = "none";
+}
+
+
+
+//설명 닫기
+function hideText(){
+    document.getElementById("desc").style.display="none";
+
+    document.getElementById("open").style.display="block";
+}
